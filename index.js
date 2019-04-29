0d162385da74244cd816cd2eac89e6f0dba3b192
Anarchy <cardinal.florian.erwan@gmail.com>
2019-04-29 22:21:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const conf = require('./conf.json'),
       discord = require('discord.js'),
       client = new discord.Client(),
       play = require('audio-play'),
-      load = require('audio-loader');
+      load = require('audio-loader'),
+      dev = {
+      	0: { name: 'Anarchy', func: 'développeur' },
+      	1: { name: 'DOC', func: 'testeur' },
+      	lastupt: '29/04/2019'
+      }
 
 function keyWord(msg) {
 	const word = Array(
@@ -49,40 +54,62 @@ catch(e) { console.log(`Connexion Failed !\nERROR : ${e}`); }
 
 var helper = Array( // Arguments de Commandes
 	`Bot ${conf.name.toUpperCase()}`,
-	`\nSyntax : !${conf.name} <arg>`,
-	"\n\nCommande Système",
+	`\n\nCommande Système\t\t[Syntax: !${conf.name} <arg>]`,
 	"----------------",
-	"\n -h\t Affiche l'aide de commande",
-	" -p\t Affiche le ping du bot",
-	` -d\t Demande à ${conf.name.toUpperCase()} la date d'aujourd'hui`,
-	` -t\t Demande à ${conf.name.toUpperCase()} l'heure à la miliseconde`,
-	"\n\nCommande Personnelle",
+	"\n --help\t Affiche l'aide de commande",
+	" --ping\t Affiche le ping du bot",
+	` --date\t Demande à ${conf.name.toUpperCase()} la date d'aujourd'hui`,
+	` --time\t Demande à ${conf.name.toUpperCase()} l'heure à la miliseconde`,
+	"\n\nCommande Jeu de Rôle\t[Syntax: !rp <arg>]",
 	"--------------------",
-	`\n patpat\t Applique un blush à ${conf.name.toUpperCase()}`
-), arg = '```';
-for(var i = 0; i < helper.length; i++) { arg += `${helper[i]}\n`; }
-arg += '```';
+	`\n <x>d<y>\t Demande à ${conf.name.toUpperCase()} de lancer x fois un dé de y faces`,
+	`\n\nCommande Personnelle\t[Syntax: !${conf.name} <arg>]`,
+	"--------------------",
+	`\n patpat\t Applique un blush à ${conf.name.toUpperCase()}`,
+	`\n\nConçu par: ${dev[0].name} & ${dev[1].name}`
+), about = Array(
+	`Bot ${conf.name.toUpperCase()} conçu par:`,
+	`\t${dev[0].name} <${dev[0].func}>`,
+	`\t${dev[1].name} <${dev[1].func}>`,
+	`\ndernière MàJ: ${dev.lastupt}`
+), temp = '```';
+for(var i = 0; i < helper.length; i++) { temp += `${helper[i]}\n`; }
+temp += '```', helper = temp, temp = '```';
+for(var i = 0; i < about.length; i++) { temp += `${about[i]}\n`; }
+temp += '```', about = temp;
 
 client
 	.on('ready', () => { console.log(`${conf.name.toUpperCase()} was connected to ${client.user.tag} !\n`); })
 	.on('error', err => { console.log(err); })
 	.on('message', msg => {
-		var time = new Date();
+		var arg = msg.content.split(' '),
+		    time = new Date();
 		
-		switch(msg.content) {
-			case `!${conf.name} -h`: msg.channel.send(arg); break; // Commande Système
-			case `!${conf.name} -p`: msg.channel.send(`:hourglass_flowing_sand: ${client.ping}ms`); break;
-			case `!${conf.name} -d`: msg.channel.send(`:date: ${time.getDay()+21}/${time.getMonth()+1}/20${time.getYear()-100}`); break;
-			case `!${conf.name} -t`: msg.channel.send(`:clock: ${time.getHours()}h ${time.getMinutes()}m ${time.getSeconds()}s ${time.getMilliseconds()}ms`); break;
-			case `t!cookie ${conf.id}`: msg.channel.send(`Merci ${msg.author} !`); break; // Réponse Humaine
-			case `!${conf.name} patpat`: msg.channel.send(`*${conf.name.toUpperCase()} is blushing.*`); break; // Réponse Perso
-			default:
-				console.log(`\nSERVER\t: ${msg.guild.id}\nUSER\t: ${msg.author}\nMSG\t: ${msg.content}`);
-				
-				if(keyWord(msg)) { // Message à envoyer
-					console.log("[ WARN ] - IDALE requise.");
-					load(['son.mp3'], { from: './' }).then(function(buffer) { play(buffer[0], { loop: false }) });
-				} break;
+		if(arg[0] === `!${conf.name}`) {
+			switch(arg[1]) {
+				case '--help': msg.channel.send(helper); break; // Commande Système
+				case '--about': msg.channel.send(about); break;
+				case '--ping': msg.channel.send(`:hourglass_flowing_sand: ${client.ping}ms`); break;
+				case '--date': msg.channel.send(`:date: ${time.getDay()+21}/${time.getMonth()+1}/20${time.getYear()-100}`); break;
+				case '--time': msg.channel.send(`:clock: ${time.getHours()}h ${time.getMinutes()}m ${time.getSeconds()}s ${time.getMilliseconds()}ms`); break;
+				case 'patpat': msg.channel.send(`*${conf.name.toUpperCase()} is blushing.*`); break; // Réponse Perso
+			}
+		}
+		else if(arg[0] === '!rp') {
+			var x = arg[1].split('d'), y, z = '', result = 0;
+			for(var i = 0; i < x[0]; i++) {
+				y = Math.floor(Math.random()*Math.floor(parseInt(x[1])+1)), result += y, z += `${y} `;
+				if(i < x[0]-1) { z += '; '; }
+			} msg.channel.send(`Liste: ${z}\nTotal: ${result}`);
+		}
+		else if(msg.content === `t!cookie ${conf.id}`) { msg.channel.send(`Merci ${msg.author} !`); } // Réponse Humaine
+		else {
+			console.log(`\nSERVER\t: ${msg.guild.id}\nUSER\t: ${msg.author}\nMSG\t: ${msg.content}`);
+			
+			if(keyWord(msg)) { // Message à envoyer
+				console.log("[ WARN ] - IDALE requise.");
+				load(['son.mp3'], { from: './' }).then(buffer => { play(buffer[0], { loop: false }) });
+			}
 		}
 	});
 
